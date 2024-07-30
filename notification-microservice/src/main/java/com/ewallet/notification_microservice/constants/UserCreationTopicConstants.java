269238a0012d07cf04d1bf562eb3e9baf3eb9c50
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-07-30 20:37:32 +0530

    configured mailtrap for sending email when new user gets created

---
+++ notification-microservice/src/main/java/com/ewallet/notification_microservice/constants/UserCreationTopicConstants.java
@@ -0,0 +1,10 @@
+package com.ewallet.notification_microservice.constants;
+
+// since by default all fields are "public static final" in interface, so we used them
+// else we could have used class also and in front of each field add "public static final"
+
+public interface UserCreationTopicConstants {
+    String NAME = "NAME";
+
+    String EMAIL = "EMAIL";
+}
--- notification-microservice/src/main/java/com/ewallet/notification_microservice/consumer/UserCreationConsumer.java
+++ notification-microservice/src/main/java/com/ewallet/notification_microservice/consumer/UserCreationConsumer.java
@@ -1,8 +1,15 @@
 package com.ewallet.notification_microservice.consumer;
 
 import com.ewallet.notification_microservice.constants.KafkaConstants;
+import com.ewallet.notification_microservice.constants.UserCreationTopicConstants;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 // we can use either of "@Service or @Component"
@@ -11,8 +18,38 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class UserCreationConsumer {
+    ObjectMapper objectMapper;
+    JavaMailSender javaMailSender; // dependency from spring starter
+
+    @Autowired
+    public UserCreationConsumer(ObjectMapper objectMapper, JavaMailSender javaMailSender) {
+        this.objectMapper = objectMapper;
+        this.javaMailSender = javaMailSender;
+    }
+
     @KafkaListener(topics = KafkaConstants.USER_CREATION_TOPIC, groupId = "notification-group")
     public void userCreated(String msg){
         log.info("user created msg received: {}", msg);
+
+        try {
+            ObjectNode objectNode = objectMapper.readValue(msg, ObjectNode.class);
+
+            String name = String.valueOf(objectNode.get(UserCreationTopicConstants.NAME));
+            String email = String.valueOf(objectNode.get(UserCreationTopicConstants.EMAIL));
+
+            // dependency from spring starter
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setFrom("java_ewalletservice@gmail.com");
+            simpleMailMessage.setTo(email);
+            simpleMailMessage.setSubject("Welcome To Java E-wallet");
+            simpleMailMessage.setText("Hey, " + name + "! Welcome To Java E-wallet");
+
+            javaMailSender.send(simpleMailMessage);
+
+            log.info("User creation mail sent: {}", simpleMailMessage);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
